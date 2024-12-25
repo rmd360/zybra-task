e01300bbd9d17a145c4983e61aecde0f451c8819
rmd360 <ravidudhat360@gmail.com>
2024-12-25 12:02:52 +0530

    fix lint

--- components/data-table/data-table.tsx
+++ components/data-table/data-table.tsx
@@ -114,16 +114,13 @@ export  function DataTable<TData, TValue>({
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
         <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
+         onClick={() => table.previousPage()}
+   
           disabled={!table.getCanPreviousPage()}
         >
           Previous
         </Button>
         <Button
-          variant="outline"
-          size="sm"
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
         >
